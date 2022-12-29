9dc3071e267d5991edac17b1987a0ddeeee302cb
Júlia Nascimento <juliavicnas@gmail.com>
2022-12-28 21:49:01 -0300

    up

---
+++ publc/js/home.js
@@ -0,0 +1,24 @@
+const myModal = new bootstrap.Modal("#transaction-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+checkLogged();
+
+function checkLogged() {
+  if (session) {
+    sessionStorage.getItem("logged", session);
+    logged = session;
+  }
+
+  if (!logged) {
+    window.location.href = "index.html";
+    return;
+  }
+
+  const dataUser = localStorage.getItem(logged);
+  if (dataUser) {
+    data = JSON.parse(dataUser);
+  }
+
+  console.log(data);
+}
